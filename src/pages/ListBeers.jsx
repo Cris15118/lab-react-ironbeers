09e4b44005b0d6672ec139dd4ac01d6f97ec2dda
Cristina Tabares <cristina_tabares@hotmail.es>
2023-05-27 10:10:30 +0200

    iteracion 3 terminada

--- src/pages/ListBeers.jsx
+++ src/pages/ListBeers.jsx
@@ -1,29 +1,58 @@
 import { Link, useNavigate } from "react-router-dom"
 import Homeimg from "../assets/home.png"
 import axios from "axios"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import {ClockLoader} from "react-spinners"
 
 
 function ListBeers() {
   const navigate = useNavigate()
-  const {listado, setListado}= useState(null)
+  const {listado, setListado}= useState([])
+  const {isLoading, setIsLoading}= useState(true)
 
 
   const getData = async()=>{
     try{
-      const response = await axios.get("https://ih-beers-api2.herokuapp.com/beers/beers")
+      const response = await axios.get("https://ih-beers-api2.herokuapp.com/beers")
         console.log(response)
+        setListado(response.data)
+        setIsLoading(false)
     }catch(err){
       navigate("/error")
     }
   }
+
+  useEffect(()=>{
+    getData()
+  }, [])
+
+  if(isLoading === true){
+    return(
+      <div>
+        <ClockLoader/>
+      </div>
+    )
+  }
+
   return (
     <div>
       <header>
-         <Link className={"navbar"} to={"/"}> <img src={Homeimg} alt="home" /> </Link>
+         <Link className={"navbar"} to={"/"}> <img src={Homeimg} alt="home" width={"40"}/> </Link>
          </header>
-
-
+<h3>Listado de Cervezas</h3>
+    
+      
+       {listado.map((eachBeer)=>{
+        return(
+          <div key={eachBeer._id}>
+          <img src={eachBeer.image_url} alt="beer" width={100} />
+          <h5><Link to={`/beers/${eachBeer._id}`}> {eachBeer.name} </Link> </h5>
+          <h5>{eachBeer.tagline} </h5>
+          <h5>{eachBeer.contributed_by} </h5>
+       </div>
+        )
+      })}
+    
     </div>
   )
 }
